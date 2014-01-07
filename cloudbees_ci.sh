12a9b1978c18dfb6b9c2e47646ce24d1c19130b6
Ilia Gilderman <iliag@vmware.com>
2014-01-06 18:59:54 -0800

    Add cloudbees script for nurego ruby

---
+++ cloudbees_ci.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export TZ=/usr/share/zoneinfo/America/Los_Angeles
+curl -s -o use-ruby https://repository-cloudbees.forge.cloudbees.com/distributions/ci-addons/ruby/use-ruby
+RUBY_VERSION=1.9.3-p327 \
+ source ./use-ruby
+
+gem install --conservative bundler
+bundle install
+
+rake spec:unit
