6adfc6b5969ef0d06684ede294678a227a6ea107
Ilia Gilderman <ilia.gilderman@openskymarkets.com>
2014-05-20 16:32:46 -0700

    WIP2

---
+++ spec/integration/entitlements_spec.rb
@@ -0,0 +1,30 @@
+require File.join(File.dirname(__FILE__), 'spec_helper')
+
+describe "Entitlements" do
+  before(:all) do
+    setup_nurego_lib
+    setup_login_and_login
+  end
+
+  it "can fetch the entitlement" do
+    # TODO Create a real entitlements here
+
+    customer = Nurego::Customer.me
+    organization = customer.organizations[0]
+    ents = organization.entitlements
+
+    customers_ent = organization.entitlements('imported_customers')
+
+    feature_id = customers_ent[0][:id]
+    max_amount = customers_ent[0][:max_allowed_amount]
+    ent = Nurego::Entitlement.new({id: organization[:id]})
+
+    ent.set_usage(feature_id, max_amount - 1)
+
+    allowed = ent.is_allowed(feature_id, 1)
+    puts "#{allowed.inspect}"
+
+    allowed = ent.is_allowed(feature_id, 2)
+    puts "#{allowed.inspect}"
+  end
+end
