ee67f6dd6049c0f59ac7b067800e96686912da2b
anatb <anat.birnboim@openskymarkets.com>
2014-05-22 02:37:36 -0700

    api and test fixes [#71825840]

--- lib/nurego/connector.rb
+++ lib/nurego/connector.rb
@@ -1,7 +1,5 @@
 module Nurego
   class Connector < APIResource
-    include Nurego::APIOperations::Create
-    include Nurego::APIOperations::Update
     include Nurego::APIOperations::List
   end
 end
